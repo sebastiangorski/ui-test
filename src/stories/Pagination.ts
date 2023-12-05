942835e537b92de4e8ea5af0c6dced6bf6a47662
sebastiangorski <gorskisebastian@me.com>
2023-12-05 01:22:29 +0100

    Cleanup

--- src/stories/Pagination.ts
+++ src/stories/Pagination.ts
@@ -179,10 +179,6 @@ export class WcPagination extends LitElement {
       this.currentPage = page;
       this.dispatchEvent(new CustomEvent('pagination-change', { detail: this.currentPage }));
     }
-
-    if (typeof page === "string") {
-      this.currentPage = this.currentPage + 1;
-    }
   }
 }
 
