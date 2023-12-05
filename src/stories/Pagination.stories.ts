232562ecfbecc098e84cb8d701aeae495f7f32a8
sebastiangorski <gorskisebastian@me.com>
2023-12-05 09:33:04 +0100

    Adjust Pagination story

--- src/stories/Pagination.stories.ts
+++ src/stories/Pagination.stories.ts
@@ -1,20 +1,11 @@
 import './Pagination';
 import {ButtonStyle, WcPagination} from "./Pagination";
 import {html} from "lit";
+import {Meta, StoryObj} from "@storybook/html";
 
-export default {
+const meta: Meta<WcPagination> = {
   title: 'wc-pagination',
   component: 'wc-pagination',
-  argTypes: {
-    buttonStyle: {
-      options: [ButtonStyle.default, ButtonStyle.circle],
-      control: { type: 'radio' },
-    },
-    textControls: {
-      options: [true, false],
-      control: { type: 'boolean' },
-    },
-  },
   parameters: {
     docs: {
       description: {
@@ -33,6 +24,71 @@ export default {
         },
       ],
     },
+    actions: {
+      handles: ['pagination-change'],
+      onClick: 'clicked'
+    },
+  },
+  argTypes: {
+    currentPage: {
+      description: 'Currently set pagination page',
+      defaultValue: 1,
+      control: { type: 'number', min: 0},
+    },
+    totalPages: {
+      description: 'Max number of pagination pages available',
+      defaultValue: 1,
+      control: { type: 'number', min: 1},
+    },
+    pagesToShow: {
+      description: 'Number of pages to display before elipsis',
+      defaultValue: 3,
+      control: { type: 'number', min: 0},
+    },
+    buttonStyle: {
+      options: [ButtonStyle.default, ButtonStyle.circle],
+      control: { type: 'radio' },
+      description: 'Square or circle styled buttons',
+      defaultValue: ButtonStyle.default
+    },
+    textControls: {
+      options: [true, false],
+      control: { type: 'boolean' },
+      description: 'Display control buttons with arrow icons or text',
+      defaultValue: false
+    },
+    withElipsis: {
+      description: 'Display pagination with elipsis',
+      defaultValue: false
+    },
+  },
+};
+
+export default meta;
+type Story = StoryObj<WcPagination>;
+
+export const Default: Story = {
+  args: {
+    currentPage: 1,
+    totalPages: 10,
+    pagesToShow: 3,
+    buttonStyle: ButtonStyle.default,
+    textControls: false,
+    withElipsis: false
+  },
+};
+
+export const Elipsis: Story = {
+  args: {
+    ...Default.args,
+    withElipsis: true,
+  },
+};
+
+export const TextControls: Story = {
+  args: {
+    ...Default.args,
+    textControls: true,
   },
 };
 
@@ -44,6 +100,7 @@ export const Interactive = (args: WcPagination) => html`
     buttonStyle="${args.buttonStyle}"
     .textControls="${args.textControls}"
     .withElipsis="${args.withElipsis}"
+    @pagination-change="${(e: CustomEvent<number>) => console.log('Page changed:', e.detail)}"
   ></wc-pagination>`;
 
 Interactive['args'] = {
@@ -53,4 +110,4 @@ Interactive['args'] = {
   buttonStyle: ButtonStyle.default,
   textControls: false,
   withElipsis: false
-}
+};
